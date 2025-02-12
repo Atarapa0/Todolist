37df94dbcd2c39c4d70b669311709b3dda43d258
Furkan Erdoğan <furkaan.er@outlook.com>
2025-02-12 19:33:27 +0300

    Card oluşturuldu

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import '../constants/constans.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,15 +10,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    double devicewith = MediaQuery.of(context).size.width;
-    double deviceheight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Container(
-        width: devicewith,
-        height: deviceheight * 0.3,
-        color: Colors.blue,
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: HexColor(backgroundColor),
+        body: Column(
+          children: [
+            Container(
+              width: deviceWidth,
+              height: deviceHeight * 0.3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/Header/Header.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      'Date',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Text(
+                      'Todo List',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.menu_open_outlined,
+                            size: 50,
+                          ),
+                          Text(
+                            'Study Lesson',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Checkbox(
+                              value: isChecked,
+                              onChanged: (val) => {
+                                    setState(() {
+                                      isChecked = val!;
+                                    })
+                                  }),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
